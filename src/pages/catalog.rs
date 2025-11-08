96e4f2bb3caf0a62fc11e5a1fd3b25854f0f0c3c
Sootpaws <sootpaws@proton.me>
2025-11-08 18:22:21 -0500

    Mural cards

--- src/pages/catalog.rs
+++ src/pages/catalog.rs
@@ -4,6 +4,6 @@ use axum::response::Response;
 use upon::value;
 
 pub async fn page(State(data): State<crate::data::Data>) -> Response {
-    let murals = data.murals.values().collect::<Vec<_>>();
+    let murals = data.murals.iter().collect::<Vec<(_, _)>>();
     template("catalog", value! { murals: murals })
 }
